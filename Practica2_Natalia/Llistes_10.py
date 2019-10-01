41798d4e68649091243a203fc9b24497f771c861
NsuarezB <nataliasuarezbenitez@gmail.com>
2019-10-01 19:51:42 +0200

    Practica2 de la Natalia

---
+++ Practica2_Natalia/Llistes_10.py
@@ -0,0 +1,6 @@
+
+import random
+
+llista = [random.randint(0,100),random.randint(0,100),random.randint(0,100),random.randint(0,100),random.randint(0,100),random.randint(0,100),random.randint(0,100),random.randint(0,100),random.randint(0,100),random.randint(0,100)]
+
+print(llista)
